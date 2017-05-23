755419a9f1649c4b53c26eab56b3dd840d7604c8
Nicolas Briand <nbriand@telelogos.com>
2017-05-23 17:38:31 +0200

    add ?????

--- app/tl-router/tl-router.ts
+++ app/tl-router/tl-router.ts
@@ -7,6 +7,8 @@ const app = angular.module('tl-router', ['ui.router']);
 
 declare const require;
 const tlHomeUrl = require('./tmpl/home.html');
+// ça ne marche pas mais j'ai quand meme essayé
+// import * as tlHomeUrl from './tmpl/home.html';
 const tlProductsUrl = require('./tmpl/products.html');
 const tlServicesUrl = require('./tmpl/services.html');
 const tlContactUrl = require('./tmpl/contact.html');
