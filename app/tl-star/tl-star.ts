6732096f8ac1e2778c201cd15016f4667b5a269c
Nicolas Briand <nbriand@telelogos.com>
2017-05-24 10:34:05 +0200

    add tl-star yellow white

--- app/tl-star/tl-star.ts
+++ app/tl-star/tl-star.ts
@@ -9,15 +9,26 @@ class TlStarCtrl {
 	constructor() {
 		console.log('TlStarCtrl', arguments);
 	}
+
+	public getImageName(i: number): string {
+		if (i <= 3) {
+			return yellowStarUrl;
+		}
+		return whiteStarUrl;
+
+	}
 }
 declare const require;
 const tlStarUrl = require('./tl-star.html');
+const yellowStarUrl = require('./img/yellow_star.png');
+const whiteStarUrl = require('./img/white_star.png');
 
 app.directive('tlStar', () => {
 	return {
 		restrict: 'E',
 		templateUrl: tlStarUrl,
 		controller: TlStarCtrl,
+		controllerAs: '$ctrl',
 	};
 });
 
